02ac26eaca6cd73f73484a1134a70f52c47834f1
Graudss <yusuf.guner.766@gmail.com>
2022-12-20 22:08:47 +0300

    update
    update

--- dersbtk/Constructors/Program.cs
+++ dersbtk/Constructors/Program.cs
@@ -17,14 +17,16 @@ namespace Constructors
             Product product = new Product { Id = 1, Name = "Leptop" };
             Product prodtct2 = new Product(2, "Computer");
 
-            EmployManager emplymoanger = new EmployManager(new DatabaseLogger);
-           
-            emplymoanger.add();
+            //EmployManager emplymoanger = new EmployManager(new DatabaseLogger);       
+           // emplymoanger.add();
+
+
             Console.ReadLine();
         }
     }
     class CustomerManager{
         private int _count=15;
+
         public CustomerManager(int count)
         {
             _count = count;
@@ -90,5 +92,31 @@ namespace Constructors
             Console.WriteLine("Added");
         }
     }
+    class BaseClass
+    {
+        private string _entitiy;
+        public BaseClass(string entity)
+        {
+            _entitiy = entity;
+        }
+        public void Mesaager()
+        {
+            Console.WriteLine("{0} message");
+        }
+    }
+    class PerspnManager: BaseClass
+    {
+        public PerspnManager(string entitiy) : base(entitiy)
+        {
+            
+        }
+        public void add()
+        {
+            Console.WriteLine("Add");
+            Mesaager();
+        }
+    }
 }
+
+
  
